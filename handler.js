053ece97658efcb7ead2e749a3c29c3a539bb69b
Jeff Leininger <jeffleus@gmail.com>
2017-04-07 15:14:24 -0700

    refactor to remove hello and change authorizer
    moved from call sheet test auth to fuel station user pool
    and, removed the hello method and function from service

--- handler.js
+++ handler.js
@@ -7,21 +7,6 @@ var sns = new AWS.SNS();
 
 var moduleName = 'sport-svc';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Hello from the sport microservice for FuelStationApp'
-//      input: event,
-    }),
-  };
-
-  callback(null, response);
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
-};
-
 module.exports.get = (event, context, callback) => {
   context.callbackWaitsForEmptyEventLoop = false;
   var response = {
